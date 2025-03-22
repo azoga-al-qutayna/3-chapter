fb901f5b51fb6b37f82ffce9e029bc3fa908df06
azoga-al-qutayna <azogaalhudu@gmail.com>
2025-03-22 14:16:20 +0600

    Add files via upload

---
+++ Java/JS.js
@@ -0,0 +1,60 @@
+// const number = 4;
+// let result = false;
+
+// while (!result) {
+//   const answer = prompt('Пароль?');
+//   if (answer === null) {
+//     break;
+//   }
+//   switch (+answer) {
+//     case number - 5:
+//     case number + 5:
+//       alert('Уже теплее!');
+//       break;
+//     case number - 2:
+//     case number + 2:
+//       alert('Горячо!');
+//       break;
+//     case number:
+//       alert('Ты угадал! Это число {040506}.');
+//       result = true;
+//       break;
+//     default:
+//       alert('Холодно!');
+//   }
+// }
+
+// $(function() {
+
+/* Menu nav toggle*/
+ $("#nav_toggle").on("click", function(event) {
+event.preventDefault();
+   $(this).toggleClass("active");
+   $("#nav").toggleClass("active");
+
+ });
+
+
+
+
+
+
+// });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
